f4f902179ffd8306238881536c37561d01fc3ecd
Marcin3232 <marcin323239@gmail.com>
2023-08-10 22:35:17 +0200

    v1

--- src/app/core/services/game.service.ts
+++ src/app/core/services/game.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { BoardTile } from '../models/board-tile.model';
+import { State } from '../models/state.enum';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GameService {
+  private readonly boardLength: number = 9;
 
-  constructor() { }
+  constructor() {}
+
+  createBoard() {
+    let board = [];
+    for (let i: number = 0; i < this.boardLength; i++) {
+      board.push(new BoardTile(i, State.DEFAULT));
+    }
+    return board;
+  }
 }
